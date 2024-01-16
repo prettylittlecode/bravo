e98b447c307a7582fe263b5688c91b3256f05adc
prettylittlecode <116849033+prettylittlecode@users.noreply.github.com>
2024-01-16 13:13:44 +1200

    Update explore.js

--- explore.js
+++ explore.js
@@ -14,8 +14,10 @@ fetch('explore.json')
         <p>${product.description}</p>
         <a <button="" href="${product.url} type="button" style="height: 45px;padding: 0px;text-decoration: none;color: white;text-align: center;">
            <img alt="Image description" style="height: 30px;" src="https://i.postimg.cc/fbgg9FHp/ariaplus-blue-square-full-back1.jpg"> <p class="like-number" style="padding: 0px;margin: 0px;width: 30px;display: flex;margin-left: 35px;margin-top: -25px;"> X</p></a>
-        <a class="comment" href="${product.url}" target="_blank">Comments</a>
-        <a class="share" href="${product.url}" target="_blank">Share</a>
+        <a <button="" href="${product.url} type="button" style="height: 45px;padding: 0px;text-decoration: none;color: white;text-align: center;">
+           <img alt="Image description" style="height: 30px;" src="https://i.postimg.cc/fbgg9FHp/ariaplus-blue-square-full-back1.jpg"> <p class="like-number" style="padding: 0px;margin: 0px;width: 30px;display: flex;margin-left: 35px;margin-top: -25px;"> X</p></a>
+        <a <button="" href="${product.url} type="button" style="height: 45px;padding: 0px;text-decoration: none;color: white;text-align: center;">
+           <img alt="Image description" style="height: 30px;" src="https://i.postimg.cc/fbgg9FHp/ariaplus-blue-square-full-back1.jpg"> <p class="like-number" style="padding: 0px;margin: 0px;width: 30px;display: flex;margin-left: 35px;margin-top: -25px;"> X</p></a>
       `;
       productsContainer.appendChild(productElement);
     });
