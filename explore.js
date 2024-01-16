51a2a89da8ac2787ae601041c8aa6e2bfacf548c
prettylittlecode <116849033+prettylittlecode@users.noreply.github.com>
2024-01-16 12:59:41 +1200

    Update explore.js

--- explore.js
+++ explore.js
@@ -12,7 +12,7 @@ fetch('explore.json')
         </a>
         <img src="${product.image}" alt="${product.name}" />
         <p>${product.description}</p>
-        <a <button="" href="${product.url}" type="button" style="height: 45px;padding: 0px;background-color: #2b2ae8;width: 300px;border-radius: 10px;">
+        <a <button="" class="like" href="${product.url}" type="button" style="height: 45px;padding: 0px;background-color: #2b2ae8;width: 300px;border-radius: 10px;">
   <img alt="Image description" style="height: 45px;padding-left: 127px;" src="https://i.postimg.cc/fbgg9FHp/ariaplus-blue-square-full-back1.jpg">
 </a>
         <a class="comment" href="${product.url}" target="_blank">Comments</a>
