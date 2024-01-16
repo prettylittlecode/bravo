ad6822493ffe9eb98dd54386227f9be224e31dac
prettylittlecode <116849033+prettylittlecode@users.noreply.github.com>
2024-01-16 13:28:15 +1200

    Update explore.js

--- explore.js
+++ explore.js
@@ -12,12 +12,14 @@ fetch('explore.json')
         </a>
         <img src="${product.image}" alt="${product.name}" />
         <p>${product.description}</p>
-        <a <button="" href="${product.url} type="button" style="height: 45px;padding: 0px;text-decoration: none;color: white;text-align: center;">
-           <img alt="Image description" style="height: 30px;" src="https://i.postimg.cc/fbgg9FHp/ariaplus-blue-square-full-back1.jpg"> <p class="like-number" style="padding: 0px;margin: 0px;width: 30px;display: flex;margin-left: 35px;margin-top: -25px;"> X</p></a>
-        <a <button="" href="${product.url} type="button" style="height: 45px;padding: 0px;text-decoration: none;color: white;text-align: center;">
-           <img alt="Image description" style="height: 30px;" src="https://i.postimg.cc/fbgg9FHp/ariaplus-blue-square-full-back1.jpg"> <p class="like-number" style="padding: 0px;margin: 0px;width: 30px;display: flex;margin-left: 35px;margin-top: -25px;"> X</p></a>
-        <a <button="" href="${product.url} type="button" style="height: 45px;padding: 0px;text-decoration: none;color: white;text-align: center;">
-           <img alt="Image description" style="height: 30px;" src="https://i.postimg.cc/fbgg9FHp/ariaplus-blue-square-full-back1.jpg"> <p class="like-number" style="padding: 0px;margin: 0px;width: 30px;display: flex;margin-left: 35px;margin-top: -25px;"> X</p></a>
+        <a <button="" href="${product.url} type=" button"="" style="height: 45px;padding: 0px;text-decoration: none;color: white;text-align: center;display: flex;">
+           <img alt="Image description" style="height: 30px;margin-top: 17px;padding-right: 5px;" src="https://i.postimg.cc/fbgg9FHp/ariaplus-blue-square-full-back1.jpg"> <p class="like-number" style="padding: 0px;margin: 0px;width: 30px;display: flex;margin-left: ;margin-top: 25px;"> X</p></a>
+
+        <a <button="" href="${product.url} type=" button"="" style="height: 45px;padding: 0px;text-decoration: none;color: white;text-align: center;display: flex;margin-left: 11%;margin-top: -27px;">
+           <img alt="Image description" style="height: 30px;" src="https://i.postimg.cc/fbgg9FHp/ariaplus-blue-square-full-back1.jpg"> <p class="like-number" style="padding: 0px;margin: 0px;width: 30px;display: flex;margin-left: 5px;margin-top: 8px;"> X</p></a>
+
+        <a <button="" href="${product.url}  type=" button"="" style="height: 45px;padding: 0px;text-decoration: none;color: white;text-align: center;display: flex;margin-left: 22%;margin-top: -45px;">
+           <img alt="Image description" style="height: 30px;" src="https://i.postimg.cc/fbgg9FHp/ariaplus-blue-square-full-back1.jpg"> <p class="like-number" style="padding: 0px;margin: 0px;width: 30px;display: flex;margin-left: 5px;margin-top: 8px;"> X</p></a>
       `;
       productsContainer.appendChild(productElement);
     });
