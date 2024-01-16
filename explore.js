30c3561f6422810df54ec3a9e5ffffbac37d89cb
prettylittlecode <116849033+prettylittlecode@users.noreply.github.com>
2024-01-16 12:51:11 +1200

    Update explore.js

--- explore.js
+++ explore.js
@@ -12,7 +12,10 @@ fetch('explore.json')
         </a>
         <img src="${product.image}" alt="${product.name}" />
         <p>${product.description}</p>
-        <a class="like" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSZE8P5VPtB4U0BHoMd7NpJerIJBPwh-Qv_GEx5DPcPtNjxg8E5V75V3oezm9xRtRD-mII&usqp=CAU" href="${product.url}" target="_blank">Like</a>
+        <img class="like" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSZE8P5VPtB4U0BHoMd7NpJerIJBPwh-Qv_GEx5DPcPtNjxg8E5V75V3oezm9xRtRD-mII&usqp=CAU" href="${product.url}" target="_blank">Like</img>
+        <a <button="" href="${product.url}" type="button" style="height: 45px;padding: 0px;background-color: #2b2ae8;width: 300px;border-radius: 10px;">
+  <img alt="Image description" style="height: 45px;padding-left: 127px;" src="https://i.postimg.cc/fbgg9FHp/ariaplus-blue-square-full-back1.jpg">
+</a>
         <a class="comment" href="${product.url}" target="_blank">Comments</a>
         <a class="share" href="${product.url}" target="_blank">Share</a>
       `;
